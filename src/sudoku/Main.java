5188c33c1e9fe5c6f5cf71efa53f4d2c23def229
Artur Grabowski <UL0234806@edu.uni.lodz.pl>
2022-02-20 22:49:34 +0100

    Initial commit

---
+++ src/sudoku/Main.java
@@ -0,0 +1,9 @@
+package sudoku;
+
+import com.example.javadesktopsudokudemo.SudokuApplication;
+
+public class Main {
+    public static void main(String[] args){
+        SudokuApplication.main(new String[]{});
+    }
+}
